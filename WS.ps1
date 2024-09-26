32af8cf07c96a8cb78fa79b2e331b6a3b81d2647
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-09-26 01:19:01 -0300

    now windows terminal is opened with 3 tabs in the working directory for easy multitasking

--- WS.ps1
+++ WS.ps1
@@ -135,7 +135,10 @@ if ($selectedWorkspace.name -match '\bship\b' -or $selectedWorkspace.name -eq 'D
             Start-Process -FilePath "code" -ArgumentList $selectedFolder -WindowStyle Hidden
             
             # Open PowerShell in the selected folder
-            Start-Process -FilePath "powershell.exe" -WorkingDirectory $selectedFolder -WindowStyle Maximized
+            #Start-Process -FilePath "powershell.exe" -WorkingDirectory $selectedFolder -WindowStyle Maximized
+
+            # Open Windows Terminal in the selected folder with 3 tabs
+            Start-Process -FilePath "wt.exe" -ArgumentList "-d `"$selectedFolder`" ; new-tab -d `"$selectedFolder`" ; new-tab -d `"$selectedFolder`"" -WindowStyle Maximized
         }
     }
 }
